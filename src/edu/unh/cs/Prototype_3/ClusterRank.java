9d3e452c1e2bf1d27ebc187f4cc1c9c5a9f1296d
tarunprasadg92 <tarunprasadg92@gmail.com>
2018-04-23 03:02:36 -0400

    Add Weighted PageRank using common entities

---
+++ src/edu/unh/cs/Prototype_3/ClusterRank.java
@@ -0,0 +1,258 @@
+package edu.unh.cs.Prototype_3;
+
+import edu.unh.cs.treccar_v2.Data;
+import edu.unh.cs.treccar_v2.read_data.CborFileTypeException;
+import edu.unh.cs.treccar_v2.read_data.CborRuntimeException;
+import edu.unh.cs.treccar_v2.read_data.DeserializeData;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.util.List;
+import java.util.Map;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.io.StringReader;
+import org.json.simple.parser.*;
+import java.util.LinkedHashMap;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.ObjectOutputStream;
+
+/*
+ * @author Tarun Prasad
+ * 
+ * Implementation of Weighted PageRank to rank paragraphs in each cluster
+ */
+
+public class ClusterRank
+{
+	HashMap<String, ArrayList<ArrayList<String>>> cluster_data;
+	HashMap<String, ArrayList<HashMap<String, Double>>> ranked_cluster_data;
+	
+	String index_path;
+	String curl_path;
+	String cluster_file;
+	String run_file;
+	
+	/*
+	 * Class to create paragraph ID as query and paragraph text as result
+	 */
+	static class MyQueryBuilder 
+	{
+		private final StandardAnalyzer analyzer;
+		private List<String> tokens;
+		 
+		public MyQueryBuilder(StandardAnalyzer standardAnalyzer) 
+		{
+			analyzer = standardAnalyzer;
+			tokens = new ArrayList<>(128);
+		}
+		
+		public BooleanQuery toIDQuery(String queryString) throws IOException 
+		{
+			TokenStream tokenStream = analyzer.tokenStream("paragraphid", new StringReader(queryString));
+			tokenStream.reset();
+			tokens.clear();
+			 
+			while(tokenStream.incrementToken()) 
+			{
+				final String token = tokenStream.getAttribute(CharTermAttribute.class).toString();
+				tokens.add(token);
+			}
+			 
+			tokenStream.end();
+			tokenStream.close();
+			 
+			BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder();
+			for (String token:tokens)
+			{
+				booleanQuery.add(new TermQuery(new Term("paragraphid", token)), BooleanClause.Occur.SHOULD);
+			}
+			 
+			return booleanQuery.build();
+		}
+	}
+	
+	public void checkReadData()
+	{
+		for (String page_id : cluster_data.keySet())
+		{
+			ArrayList<ArrayList<String>> clusters_in_page = cluster_data.get(page_id);
+			for (ArrayList<String> cluster : clusters_in_page)
+			{
+				System.out.println("\nNo of paragraphs in cluster : " + cluster.size());
+				for (String paragraph_id : cluster)
+				{
+					System.out.print(paragraph_id + ", ");
+				}
+			}
+		}
+	}
+	
+	public void rankClusters() throws IOException, ParseException
+	{
+		IndexSearcher searcher = setUpIndexSearcher(index_path, "paragraph.lucene");
+		searcher.setSimilarity(new BM25Similarity());
+		final MyQueryBuilder query_builder = new MyQueryBuilder(new StandardAnalyzer());
+		
+		// checkReadData();
+		System.out.println("\nRanking the clusters...");
+		ranked_cluster_data = new HashMap<String, ArrayList<HashMap<String, Double>>>();
+		int count = 0;
+		
+		for (String page_id : cluster_data.keySet())
+		{
+			ArrayList<ArrayList<String>> clusters_in_page = cluster_data.get(page_id);
+			ArrayList<HashMap<String, Double>> ranked_clusters = new ArrayList<HashMap<String, Double>>();
+			
+			for (ArrayList<String> cluster : clusters_in_page)
+			{
+				LinkedHashMap<String, List<String>> entities_in_paragraph = new LinkedHashMap<String, List<String>>();
+				HashMap<String, Double> page_rank_result = new HashMap<String, Double>();
+				
+				for (String paragraph_id : cluster)
+				{
+					String query_string = paragraph_id;
+					TopDocs tops = searcher.search(query_builder.toIDQuery(query_string), 1);
+					ScoreDoc[] score_doc = tops.scoreDocs;
+					ScoreDoc score = score_doc[0];
+					
+					final Document doc = searcher.doc(score.doc);
+					final String paragraph_text = doc.getField("text").stringValue();
+					
+					EntityExtraction ee = new EntityExtraction(curl_path, paragraph_text);
+					List<String> entities_for_paragraph = ee.retrieveEntities();
+					entities_in_paragraph.put(paragraph_id, entities_for_paragraph);
+				}
+				WeightedPageRankEntity wpre = new WeightedPageRankEntity(entities_in_paragraph);
+				page_rank_result = wpre.getPageRanks();
+				ranked_clusters.add(page_rank_result);
+			}
+			ranked_cluster_data.put(page_id, ranked_clusters);
+			if (count % 25 == 0)
+				System.out.println();
+			System.out.print(".");
+			count++;
+		}
+		
+		FileWriter fw = new FileWriter("WeightedPageRankCommonEntities.run", true);
+		System.out.println("\nWriting run file...");
+		count = 0;
+		try
+		{
+			String line = null;
+			FileReader fr = new FileReader(run_file);
+			BufferedReader br = new BufferedReader(fr);
+			while ((line = br.readLine()) != null)
+			{
+				String[] splits = line.split(" ");
+				String pg_id = splits[0];
+				String para_id = splits[2];
+				String cluster_score = splits[4];
+				
+				double c_score = Double.parseDouble(cluster_score);
+				double new_score = getScore(para_id) * c_score;
+				
+				String final_string = pg_id + " Q0 " + para_id + " 0 " + new_score + " WeightedPRCommonEntities\n";
+				fw.write(final_string);
+				
+				if (count % 25 == 0)
+					System.out.println();
+				System.out.print(".");
+				count++;
+			}
+			
+			br.close();
+			fw.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public double getScore(String para_id)
+	{
+		for (String page_id : ranked_cluster_data.keySet())
+		{
+			ArrayList<HashMap<String, Double>> clusters_in_page = ranked_cluster_data.get(page_id);
+			for (HashMap<String, Double> cluster : clusters_in_page)
+			{
+				for (String paragraph_id : cluster.keySet())
+				{
+					if (paragraph_id.equals(para_id))
+						return cluster.get(paragraph_id);
+				}
+			}
+		}
+		return 0.0;
+	}
+	
+	public ClusterRank(String arg1, String arg2, String arg3, String arg4) throws IOException, ParseException
+	{
+		cluster_file = arg1;
+		index_path = arg2;
+		curl_path  = arg3;
+		run_file = arg4;
+		
+		// System.out.println("Cluster file : " + cluster_file + " Index Path : " + index_path + " Curl Path : " + curl_path);
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(cluster_file)));
+		try
+		{
+			cluster_data = (HashMap<String, ArrayList<ArrayList<String>>>)ois.readObject();
+		}
+		catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		ois.close();
+		
+		rankClusters();		
+	}
+	
+	/*
+	 * Function to set up the index searcher
+	 */
+	private static IndexSearcher setUpIndexSearcher(String index_path, String type) throws IOException 
+	{
+		Path path = FileSystems.getDefault().getPath(index_path, type);
+		Directory index_directory = FSDirectory.open(path);
+		IndexReader reader = DirectoryReader.open(index_directory);
+		return new IndexSearcher(reader);
+	}
+	
+	public static void main(String[] args) throws IOException, ParseException
+	{
+		System.setProperty("file.encoding", "UTF-8");
+		
+		// Check command line arguments
+		if (args.length < 4)
+		{
+			System.out.println("Command line arguments : <cluster-file> <lucene-index> <path-to-curl-script> <run-file>");
+			System.exit(-1);
+		}
+		
+		ClusterRank cr = new ClusterRank(args[0], args[1], args[2], args[3]);
+	}
+}
---
+++ src/edu/unh/cs/Prototype_3/WeightedPageRankEntity.java
@@ -0,0 +1,252 @@
+package edu.unh.cs.Prototype_3;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/*
+ * @author Tarun Prasad
+ * 
+ * Class to compute the Weighted Page Rank of paragraphs inside a given cluster
+ */
+public class WeightedPageRankEntity
+{
+	public int weight_matrix[][];
+	public double transition_probability_matrix[][];
+	public double page_rank_vector[];
+	
+	public double teleportation_probability = 0.15;
+	public boolean convergence_reached = false;
+	public int num_rows;
+	
+	public Map<String, Integer> index_positions;
+	public LinkedHashMap<String, List<String>> input;
+	public HashMap<String, Double> page_ranks;
+	
+	public WeightedPageRankEntity(LinkedHashMap<String, List<String>> in)
+	{
+		input = new LinkedHashMap<String, List<String>>();
+		input.putAll(in);
+		num_rows = input.size();
+		
+		// Initialize Weight Matrix
+		weight_matrix = new int[num_rows][];
+		for (int i = 0; i < num_rows; i++)
+		{
+			weight_matrix[i] = new int[num_rows];
+		}
+		
+		// Initialize Transition Probability Matrix
+		transition_probability_matrix = new double[num_rows][];
+		for (int i = 0; i < num_rows; i++)
+		{
+			transition_probability_matrix[i] = new double[num_rows];
+		}
+		
+		// Initialize PageRank vector
+		page_rank_vector = new double[num_rows];
+		double val = 1.0 / num_rows;
+		for (int i = 0; i < num_rows; i++)
+		{
+			page_rank_vector[i] = val;
+		}
+		
+		index_positions = new HashMap<String, Integer>();
+		int index = 0;
+		for (String nodes : input.keySet())
+		{
+			index_positions.put(nodes, index);
+			index++;
+		}
+		
+		computeWeightMatrix();
+		computeTransitionProbabilityMatrix();	
+		
+		page_ranks = new HashMap<String, Double>();
+	}
+	
+	public HashMap<String, Double> getPageRanks()
+	{
+		if (num_rows == 1)
+		{
+			page_ranks.put(input.keySet().toString(), 1.0);
+			return page_ranks;
+		}
+		
+		performIteration();
+		assignScores();
+		return page_ranks;
+	}
+	
+	public void assignScores()
+	{
+		for (String node : input.keySet())
+		{
+			double value = getRank(node);
+			page_ranks.put(node, value);
+		}
+	}
+	
+	public void performIteration()
+	{
+		double temp_vector[] = new double[num_rows];
+		for (int i = 0; i < 1; i++)
+		{
+			for (int j = 0; j < num_rows; j++)
+			{
+				for (int k = 0; k < num_rows; k++)
+				{
+					temp_vector[j] += page_rank_vector[k] * transition_probability_matrix[k][j];
+				}
+			}
+		}
+		
+		convergence_reached = true;
+		for (int m = 0; m < num_rows; m++)
+		{
+			if ((Math.abs(page_rank_vector[m] - temp_vector[m]) > 0.001))
+			{
+				convergence_reached = false;
+			}
+			page_rank_vector[m] = temp_vector[m];
+		}
+	}
+	
+	public int getNoOutlinks(int index)
+	{
+		int no_outlinks = 0;
+		for (int i = 0; i < weight_matrix[index].length; i++)
+		{
+			if (weight_matrix[index][i] != 0)
+			{
+				no_outlinks++;
+			}
+		}
+		return no_outlinks;
+	}
+	
+	public int getTotalWeight(int index)
+	{
+		int total_weight = 0;
+		for (int i = 0; i < weight_matrix[index].length; i++)
+		{
+			if (weight_matrix[index][i] != 0)
+			{
+				total_weight += weight_matrix[index][i];
+			}
+		}
+		return total_weight;
+	}
+	
+	public void computeTransitionProbabilityMatrix()
+	{
+		int i = 0;
+		for (String nodes : input.keySet())
+		{
+			int no_outlinks = getNoOutlinks(i);
+			if (no_outlinks == 0)
+			{
+				for (int j = 0; j < num_rows; j++)
+				{
+					transition_probability_matrix[i][j] = (1.0 / num_rows);
+				}
+			}
+			else
+			{
+				int total_weight = getTotalWeight(i);
+				for (int j = 0; j < num_rows; j++)
+				{
+					if (weight_matrix[i][j] != 0)
+					{
+						transition_probability_matrix[i][j] = (teleportation_probability / num_rows) + ((1.0 - teleportation_probability) * (weight_matrix[i][j] / total_weight));
+					}
+					else
+					{
+						transition_probability_matrix[i][j] = (teleportation_probability / num_rows);
+					}
+				}
+			}
+			i++;
+		}
+	}
+	
+	public void computeWeightMatrix()
+	{
+		for(Map.Entry<String, List<String>> source_entry : input.entrySet())
+		{
+			int source_id = index_positions.get(source_entry.getKey());
+			List<String> source_entities = source_entry.getValue();
+			
+			for (Map.Entry<String, List<String>> target_entry : input.entrySet())
+			{
+				int target_id = index_positions.get(target_entry.getKey());
+				List<String> target_entities = target_entry.getValue();
+				
+				int entity_count = commonEntityCount(source_entities, target_entities);
+				weight_matrix[source_id][target_id] = entity_count;
+			}
+		}
+	}
+	
+	public int commonEntityCount(List<String> source, List<String> target)
+	{
+		int count = 0;
+		for (String s_en : source)
+		{
+			if (target.contains(s_en))
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public double getRank(String node)
+	{
+		int position = index_positions.get(node);
+		return page_rank_vector[position];
+	}
+	
+	public void printPageRankVector()
+	{
+		double sum = 0.0;
+		for (int i = 0; i < num_rows; i++)
+		{
+			System.out.print(page_rank_vector[i] + " ");
+			sum += page_rank_vector[i];
+		}
+		System.out.println();
+		System.out.println("\nSum : " + sum);
+	}
+	
+	public void printTransitionProbabilityMatrix()
+	{
+		for (int i = 0; i < num_rows; i++)
+		{
+			for (int j = 0; j < num_rows; j++)
+			{
+				System.out.print(transition_probability_matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void printData()
+	{
+		for (String nodes : input.keySet())
+		{
+			System.out.print(nodes + " : ");
+			for (String linked_nodes : input.get(nodes))
+			{
+				System.out.print(linked_nodes + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public int getIndex(String node)
+	{
+		return index_positions.get(node).intValue();
+	}
+}
